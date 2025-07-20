f84fc062ed9e7d7d91cc2049fa290fa022ee4d37
Siyabonga Mnguni <99957620+poundcake010101@users.noreply.github.com>
2025-07-20 23:14:58 +0200

    updated

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+function openVideoModal() {
+                const modal = document.getElementById("videoModal");
+                const frame = document.getElementById("videoFrame");
+                frame.src = "https://www.youtube.com/embed/hR5KFCDNvMo?autoplay=1";
+                modal.style.display = "block";
+            }
+
+            function closeVideoModal() {
+                const modal = document.getElementById("videoModal");
+                const frame = document.getElementById("videoFrame");
+                frame.src = ""; // stop video
+                modal.style.display = "none";
+            }
+
+            window.onclick = function(event) {
+                const modal = document.getElementById("videoModal");
+                if (event.target === modal) {
+                closeVideoModal();
+                }
+            };
